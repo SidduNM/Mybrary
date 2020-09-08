133d1ae955c06ca0ad900fc677e62671c5f91ff3
Siddu <siddunm@example.com>
2020-09-08 15:16:20 +0530

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All Author Route
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    // console.log('Name...', req.query.name)
+    // console.log('ff', req.query)
+    if(req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+         })
+
+    } catch (e) {
+        res.redirect('/')
+
+    }
+})
+
+//New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+
+})
+
+//Create Authors Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+
+    } catch (e) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
+        })
+    }
+})
+module.exports = router
